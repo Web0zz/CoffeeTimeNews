b93c6187a6e9669a63b4bd19dfffe727d35a8729
Web0zz <furkan.ozen4@ogr.sakarya.edu.tr>
2021-08-06 22:24:12 +0300

    api_token header added + Dependencies versions updated

--- data/src/main/java/com/web0z/data/remote/Constant.kt
+++ data/src/main/java/com/web0z/data/remote/Constant.kt
@@ -1,6 +1,9 @@
 package com.web0z.data.remote
 
+import com.web0z.data.BuildConfig
+
+
 object Constant {
-    // TODO dummy api resource needed
     const val API_BASE_URL = "https://coffeetimenews.herokuapp.com"
+    const val API_TOKEN = "Bearer: ${BuildConfig.API_TOKEN}"
 }
--- data/src/main/java/com/web0z/data/remote/api/CoffeeTimeNewsService.kt
+++ data/src/main/java/com/web0z/data/remote/api/CoffeeTimeNewsService.kt
@@ -1,11 +1,15 @@
 package com.web0z.data.remote.api
 
+import com.web0z.data.remote.Constant.API_TOKEN
 import com.web0z.data.remote.model.ArticlesResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 
+
 interface CoffeeTimeNewsService {
+    @Headers(API_TOKEN)
     @GET("/article/{category}")
     suspend fun getArticlesByCategory(
         @Path("category") category: String
