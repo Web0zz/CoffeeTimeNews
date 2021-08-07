222a2530d7888a0bb7f85f2f9f9ae9cea760965c
Web0zz <furkan.ozen4@ogr.sakarya.edu.tr>
2021-08-07 23:47:11 +0300

    State enum added

--- data/src/main/java/com/web0z/data/remote/model/BaseResponse.kt
+++ data/src/main/java/com/web0z/data/remote/model/BaseResponse.kt
@@ -1,6 +1,10 @@
 package com.web0z.data.remote.model
 
 interface BaseResponse {
-    val status: Int
+    val status: State
     val message: String
+}
+
+enum class State {
+    SUCCESS, NOT_FOUND, FAILED, UNAUTHORIZED
 }
