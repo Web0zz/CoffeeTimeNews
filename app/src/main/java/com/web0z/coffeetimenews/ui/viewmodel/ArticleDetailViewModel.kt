dbc88336f41cd1bcfb05f1db5192c68289f91773
Web0zz <furkan.ozen4@ogr.sakarya.edu.tr>
2021-08-14 20:59:44 +0300

    getArticle function redesign

--- app/src/main/java/com/web0z/coffeetimenews/ui/viewmodel/ArticleDetailViewModel.kt
+++ app/src/main/java/com/web0z/coffeetimenews/ui/viewmodel/ArticleDetailViewModel.kt
@@ -7,6 +7,7 @@ import com.web0z.coffeetimenews.di.Repository
 import com.web0z.coffeetimenews.ui.state.UiState
 import com.web0z.core.model.Article
 import com.web0z.core.repository.CoffeeTimeNewsRepository
+import com.web0z.core.repository.ResponseResult
 import dagger.Module
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
@@ -36,7 +37,23 @@ class ArticleDetailViewModel @AssistedInject constructor(
                     _state.value = UiState(loading = true, error = null, data = null)
                 }
                 .catch {
-                    _state.value = UiState(loading = false, error = it.localizedMessage, data = null)
+                    /**
+                     * If catch any error while getting data from database
+                     * it will try to take data from api by id
+                     */
+                    coffeeTimeNewsRepository.getArticleByIdFromApi(articleId)
+                        .onStart {  _state.value = UiState(loading = true, error = null, data = null) }
+                        .catch { _state.value = UiState(loading = false, error = it.localizedMessage, data = null) }
+                        .collect { articleResponse ->
+                            when(articleResponse) {
+                                is ResponseResult.Success -> {
+                                    _state.value = UiState(loading = false, error = null, data = articleResponse.data)
+                                }
+                                is ResponseResult.Error -> {
+                                    _state.value = UiState(loading = false, error = articleResponse.message, data = null)
+                                }
+                            }
+                        }
                 }
                 .collect { article ->
                     _state.value = UiState(loading = false, error = null, data = article)
